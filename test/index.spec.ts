cef1e564c410c3b2badf8fca6cf55eb1ca239f56
Adam Beeson <adambeeson@OR-TO-12118.local>
2024-05-01 20:40:36 -0600

    updated unit test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -25,5 +25,13 @@ describe('GET /', () => {
     it('should return a QR Code if bcid is type qrcode', async () => {
         const response = await supertest(app).get('/?text=978-1-56581-231-4+52250&bcid=qrcode');
         expect(response.header.bcid).to.equal('qrcode');
+        expect(response.status).to.equal(200);
+    });
+
+    it('should return a 500 if we pass an unknown bcid', async () => {
+        const badEncoderName = `SomeInvalidEncoderName`;
+        const response = await supertest(app).get(`/?text=978-1-56581-231-4+52250&bcid=${badEncoderName}`);
+        expect(response.status).to.equal(500);
+        expect(response.text).to.include(`Error processing barcode: Error: bwipp.unknownEncoder: unknown encoder name: ${badEncoderName}`)
     });
 });
